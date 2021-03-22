cfdc2b2d9ce3b90b4c5387e2ff2133511bec0f6b
hutao <Ht$19901215>
2021-03-22 19:31:55 +0800

    修改路径问题

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/ResourcesService.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/ResourcesService.java
@@ -259,9 +259,10 @@ public class ResourcesService extends BaseService {
             logger.info("creating dir: '{}/{}'", p.getKey(), p.getValue());
             String cd = currentDir + p.getKey();
             createDirectory(loginUser, p.getValue(), "", type, dirIdMap.getOrDefault(cd, pid), cd);
-            List<Resource> resources = resourcesMapper.queryResourceList(cd + p.getValue(), loginUser.getId(), 0);
+            String fullName = cd + p.getValue();
+            List<Resource> resources = resourcesMapper.queryResourceList(fullName, loginUser.getId(), 0);
             if (resources != null && resources.size() > 0) {
-                dirIdMap.put(cd, resources.get(0).getId());
+                dirIdMap.put(fullName, resources.get(0).getId());
             }
         });
         files.forEach(p -> {
