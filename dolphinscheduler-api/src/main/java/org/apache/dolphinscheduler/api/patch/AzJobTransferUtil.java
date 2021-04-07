8a6fc33c6d97b90584ffeb55e28019fb62e3308e
hutao <Ht$19901215>
2021-04-07 18:54:08 +0800

    支持上传azkaban的zip包

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/patch/AzJobTransferUtil.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/patch/AzJobTransferUtil.java
@@ -1,10 +1,13 @@
 package org.apache.dolphinscheduler.api.patch;
 
+import lombok.Cleanup;
 import org.apache.commons.compress.archivers.zip.ZipFile;
+import org.apache.commons.io.IOUtils;
 import org.apache.dolphinscheduler.api.patch.utils.ZipUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 /**
@@ -20,7 +23,8 @@ public class AzJobTransferUtil {
         String[] split = fileName.split("\\.");
         String path1 = "/tmp/"+split[0]+"-tmp1."+split[1];
         String path2 = "/tmp/"+split[0]+"-tmp2."+split[1];
-        file.transferTo(new File(path1));
+        @Cleanup FileOutputStream out = new FileOutputStream(new File(path1));
+        IOUtils.copy(file.getInputStream(),out);
         ZipUtils.transform(path1,path2);
         return JobTransfer.trans(path2);
     }
