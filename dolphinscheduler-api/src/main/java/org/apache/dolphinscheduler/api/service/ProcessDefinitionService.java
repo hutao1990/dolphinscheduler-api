b9a41cad8d6fa67844484e093a97fda344226155
hutao <Ht$19901215>
2021-05-13 17:49:20 +0800

    修改az的job文件名的日期格式

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/ProcessDefinitionService.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/ProcessDefinitionService.java
@@ -789,7 +789,7 @@ public class ProcessDefinitionService extends BaseDAGService {
                 try {
                     if (!resources.isEmpty()) {
                         Resource resource = resources.get(0);
-                        ZipMultiPartFile zipMultiPartFile = new ZipMultiPartFile(file.getOriginalFilename().replace(".", DateUtils.getCurrentTime("yyyy_MM_dd_HH_mm_ss") + "."), file.getInputStream());
+                        ZipMultiPartFile zipMultiPartFile = new ZipMultiPartFile(file.getOriginalFilename().replace(".", DateUtils.getCurrentTime("_yyyyMMddHHmmss") + "."), file.getInputStream());
                         resourcesService.createResource(loginUser, "", "", ResourceType.FILE, new MultipartFile[]{zipMultiPartFile}, resource.getId(), resource.getFullName());
                     }
                 } catch (IOException e) {
